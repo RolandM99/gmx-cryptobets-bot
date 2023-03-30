55a6aaefdf25fd0181a395c1d7190bf6ae0626b6
rolandm99 <manfulmweze99@gmail.com>
2023-03-30 16:56:14 +0200

    Initial commit

---
+++ src/types/interface.ts
@@ -0,0 +1,13 @@
+export interface IOrder {
+    path: any,
+    amountIn: any,
+    indexToken: string,
+    minOut: any,
+    sizeDelta: any,
+    collateralDelta: any,
+    acceptablePrice: any,
+    executionFee: any,
+    callbackTarget: any,
+    isLong: boolean,
+    referralCode: string
+}
